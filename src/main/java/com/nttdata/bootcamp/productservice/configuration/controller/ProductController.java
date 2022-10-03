53ffb976a6694457862b4ee0f63154ebf9c3fd4c
Gabriel Sánchez <gabionezero@gmail.com>
2022-10-03 13:05:39 +0200

    Add @Autowired
    Add @autowired and updates method for ProductServiceI

--- src/main/java/com/nttdata/bootcamp/productservice/configuration/controller/ProductController.java
+++ src/main/java/com/nttdata/bootcamp/productservice/configuration/controller/ProductController.java
@@ -1,12 +1,14 @@
 package com.nttdata.bootcamp.productservice.configuration.controller;
 
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import com.nttdata.bootcamp.productservice.configuration.service.ProductServiceI;
 import com.nttdata.bootcamp.productservice.configuration.service.ProductServiceImp;
 import com.nttdata.bootcamp.productservice.dto.Product;
 
@@ -14,29 +16,18 @@ import com.nttdata.bootcamp.productservice.dto.Product;
 @RequestMapping("/")
 public class ProductController {
 	
+	@Autowired
+	private ProductServiceI productService;
+	
+	
 	@GetMapping("/{id}")
-	public Product getProduct(@PathVariable Integer id) {
-		
-		ProductServiceImp product = new ProductServiceImp();
-		Product result = product.getProduct(id);
-		
-		// Respuesta.
-		System.out.println(result.getProductId());
-
-		return result;
+	public ResponseEntity<Product> getProduct(final @PathVariable int id){
+		return ResponseEntity.ok(productService.getProduct(id));
 	}
 	
-	
 	@PostMapping("/{id}")
-	public Product postProduct(@PathVariable Integer id) {
-		
-		ProductServiceImp product = new ProductServiceImp();
-		Product result = product.postProduct(id);
-		
-		// Respuesta.
-		System.out.println(result.getProductId());
-
-		return result;
+	public ResponseEntity<Product> postProduct(final @PathVariable int id){
+		return ResponseEntity.ok(productService.postProduct(id));
 	}
 
 
